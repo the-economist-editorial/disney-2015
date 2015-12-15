311a81370155e7e6f7bb04e5cd28909e3ff4e0bd
Evan Hensleigh <futuraprime@gmail.com>
2015-12-15 13:29:35 +0000

    Axes.

--- js/disney_scatter.js
+++ js/disney_scatter.js
@@ -31,7 +31,7 @@ export default class Scatter extends SVGComponent {
   static get defaultProps() {
     return {
       data : [],
-      xRange : [20, 555],
+      xRange : [20, 500],
       yRange : [350, 10]
     };
   }
@@ -42,6 +42,7 @@ export default class Scatter extends SVGComponent {
     return (<svg height="400" width="595">
       <polygon className='backdrop' points={`${x1},${y1} ${x1},${y2} ${x2},${y2} ${x2},${y1}`} />
       <g ref="x-axis"></g>
+      <g ref="y-rules" className="rules"></g>
       <g ref="y-axis"></g>
       <g ref="scatter"></g>
       <g ref="voronoi"></g>
@@ -54,16 +55,36 @@ export default class Scatter extends SVGComponent {
     var xScale = d3.time.scale().range(this.props.xRange).domain(['01/01/1937', '31/12/2015'].map(dateFormat.parse));
     var yScale = d3.scale.linear().range(this.props.yRange).domain([0,7e8]);
 
-    // making an axis!
     var xAxis = d3.svg.axis()
       .outerTickSize(0)
       .scale(xScale);
-
     var xAxisLayer = this.selectRef('x-axis')
       .attr({
         'transform' : generateTranslateString(0,y2)
-      });
-    xAxisLayer.call(xAxis);
+      })
+      .call(xAxis);
+
+    var yAxis = d3.svg.axis()
+      .outerTickSize(0)
+      .orient('right')
+      .scale(yScale);
+    var yAxisLayer = this.selectRef('y-axis')
+      .attr({
+        transform : generateTranslateString(x2,0)
+      })
+      .call(yAxis);
+
+    var yRules = d3.svg.axis()
+      .outerTickSize(0)
+      .innerTickSize(x1 - x2)
+      .orient('right')
+      .tickFormat(d => '')
+      .scale(yScale);
+    var yRuleLayer = this.selectRef('y-rules')
+      .attr({
+        transform : generateTranslateString(x2,0)
+      })
+      .call(yRules);
 
 
     var scatterLayer = this.selectRef('scatter');
@@ -76,13 +97,13 @@ export default class Scatter extends SVGComponent {
         cx : d => xScale(d[DATE]),
         cy : yScale.range()[0],
         fill : 'white',
-        'stroke-width' : 1.5,
+        'stroke-width' : 1,
         'fill-opacity' : 0.6,
         opacity : 0
       });
     scatterJoin.exit().remove();
     scatterJoin.transition().duration(350).delay(d => (xScale(d[DATE]) - x1) * 8).ease('cubic-out').attr({
-      r : 4,
+      r : 3,
       cx : d => xScale(d[DATE]),
       cy : d => yScale(d[BOX_OFFICE]),
       stroke : typeColour,
@@ -100,12 +121,12 @@ export default class Scatter extends SVGComponent {
     var voronoiJoin = voronoiLayer.selectAll('.voronoi')
       .data(voronoi(this.props.data));
 
-    voronoiJoin.enter()
-      .append('svg:path')
-      .classed('voronoi', true);
-    voronoiJoin.exit().remove();
-    voronoiJoin.attr({
-      d : d => d ? `M${d.join('L')}Z` : null
-    });
+    // voronoiJoin.enter()
+    //   .append('svg:path')
+    //   .classed('voronoi', true);
+    // voronoiJoin.exit().remove();
+    // voronoiJoin.attr({
+    //   d : d => d ? `M${d.join('L')}Z` : null
+    // });
   }
 }
